72d5ae2388b792d0b02cc6da85940cafaa07e8a1
SAldoshin <Aldoshin_Serge@mail.ru>
2020-03-05 23:38:29 +0300

    add background

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -7,6 +7,7 @@ const Aside = (props) => {
 	return (
 		<aside className="aside">
 			<Menu cls="menu_vertical_layout" menu={props.menu} />
+			<img src="./images/bckg_container.jpg" alt='main' className="aside__background" />
 		</aside>
 	)
 }
